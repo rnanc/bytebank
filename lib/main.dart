d231257b31dd1d714bd0b0e445c5fc905eaf7f06
Renan <renanfigcarneiro@gmail.com>
2020-09-30 21:34:44 -0300

    style: container style on main

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,58 @@ import 'package:flutter/material.dart';
 void main() => runApp(ByteBankApp());
 
 class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controllerAccountName = TextEditingController();
+  final TextEditingController _controllerAccountValue = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Criando Transferência'),),
-      body: Text('teste'),
-
+      appBar: AppBar(
+        title: Text('Criando Transferência'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controllerAccountName,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                  labelText: "Número da conta", hintText: "000"),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controllerAccountValue,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: "Valor",
+                  hintText: "0.00"),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {
+              final int accountNumber =
+                  int.tryParse(_controllerAccountName.text);
+              final double accountValue =
+                  double.tryParse(_controllerAccountValue.text);
+              if (accountValue != null && accountNumber != null) {
+                final transferenciaCriada =
+                    Transferencia(accountValue, accountNumber);
+              }
+            },
+            child: Text("Confirmar"),
+          )
+        ],
+      ),
     );
   }
 }
